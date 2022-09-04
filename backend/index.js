f0e88eac1c4e6f9e9dee0c98fb645e6485d5b970
Saeed Alnuaimi <saeed@nuaimi.net>
2022-09-04 15:32:31 +0400

    added routes

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,52 @@
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
-
+const credentials = require('./middleware/credentials')
+const corsOptions = require('./config/corsOptions')
+const verifyToken = require('./middleware/verifyToken')
 const app = express()
 const PORT = process.env.PORT || 3000;
 
-// Database
-require('./database/mongoose')
+//routes
+const apiRouter = require('./routes/api')
+const registerRoute = require('./routes/register')
+const loginRoute = require('./routes/login')
+const refreshRoute = require('./routes/refreshToken')
+const logutRoute = require('./routes/logout')
+const userAccountRoute = require('./routes/userAccount')
+const adminAccountRoute = require('./routes/adminAccount')
+const screeningRoute = require('./routes/screening')
+const cinemaRoute = require('./routes/cinema')
+const upcomingMovies = require('./routes/upcomingMovies')
+const moviesManagement = require('./routes/moviesManagement')
+const movieInfo = require('./routes/movieInfo')
+const cinemaHallRoute = require('./routes/cinemaHall')
 
+// Database
+require('./db/mongoose')
+
+app.use(credentials)
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cookieParser())
+
+//route middlewares
+app.use('/', apiRouter)
+app.use('/', upcomingMovies)
+app.use('/movie', movieInfo)
+app.use('/register', registerRoute)
+app.use('/login', loginRoute)
+app.use('/refresh', refreshRoute)
+app.use('/logout', logutRoute)
+app.use('/', cinemaHallRoute)
+app.use('/', screeningRoute)
+app.use(verifyToken)
+app.use('/user-account', userAccountRoute)
+app.use('/admin-account', adminAccountRoute)
+app.use('/', cinemaRoute)
+app.use('/movies', moviesManagement)
 
 //server
 app.listen(() => {
